4738cc6ed0df2049a514fae231595b35874897a5
EduardGarritsen <eduard.garritsen@student.hu.nl>
2017-10-14 16:09:41 +0200

    Commit

---
+++ Les_10/Exercise 10_1.py
@@ -0,0 +1,7 @@
+import xmltodict
+
+def processXML(filename):
+    with open(filename) as myXMLFile:
+        filecontentstring = myXMLFile.read()
+        xmldictionary = xmltodict.parse(filecontentstring)
+        return xmldictionary
